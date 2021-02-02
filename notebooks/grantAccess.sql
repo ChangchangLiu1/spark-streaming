9c0a99843565e36882543f51507fd3d007cd8af5
Changchang Liu <38497792+ChangchangLiu1@users.noreply.github.com>
2021-02-02 23:55:30 +0800

    Update: README.md notebooks

---
+++ notebooks/grantAccess.sql
@@ -0,0 +1,23 @@
+-- Databricks notebook source
+
+GRANT USAGE ON DATABASE accounting TO testgroup;
+GRANT CREATE ON DATABASE accounting TO testgroup;
+
+-- COMMAND ----------
+
+show grant on database accounting
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
+
+
+-- COMMAND ----------
+
